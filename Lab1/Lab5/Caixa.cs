a1772999e12be8f20e9d541d4ab10ea631ebd9b9
ueliton freitas <ueliton freitas>
2015-11-07 11:41:45 -0200

    Terminado Lab5

---
+++ Lab1/Lab5/Caixa.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab1.Lab5
+{
+    internal class Caixa
+    {
+        private List<int> notasDisponiveisEmCaixa;
+
+        public Caixa()
+        {
+            notasDisponiveisEmCaixa = new List<int>() { 100, 50, 20, 10 };
+        }
+
+        internal List<int> Saque(int valorDoSaque)
+        {
+            return CaixaPodePagarOhValor(valorDoSaque) ? ObtemQuantidadeDeNotas(valorDoSaque) : new List<int>();
+        }
+
+        private List<int> ObtemQuantidadeDeNotas(int valorDoSaque)
+        {
+            var valorAtualParaSerPago = valorDoSaque;
+            var notasAhSeremPagas = new List<int>();
+
+            foreach (int nota in notasDisponiveisEmCaixa)
+            {
+                var quantidadeMaximaDeNotas = CalculaQuantidadeMaximoDeNotas(valorAtualParaSerPago, nota);
+           
+                if (quantidadeMaximaDeNotas > 0)
+                {
+                    AdicionaQuantidadeDeNotas(notasAhSeremPagas, nota, quantidadeMaximaDeNotas);
+                }
+
+                valorAtualParaSerPago = valorAtualParaSerPago % (nota * quantidadeMaximaDeNotas);
+                if (valorAtualParaSerPago == 0)
+                {
+                    break;
+                }
+            }
+
+            return notasAhSeremPagas;
+        }
+
+        private int CalculaQuantidadeMaximoDeNotas(int valorParcial, int nota)
+        {
+            return valorParcial / nota;
+        }
+
+        private bool CaixaPodePagarOhValor(int valorDoSaque)
+        {
+            foreach (int nota in notasDisponiveisEmCaixa)
+            {
+                if (valorDoSaque % nota == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void AdicionaQuantidadeDeNotas(List<int> notas, int valor, int quantidadeDeNotas)
+        {
+            for (int i = 0; i < quantidadeDeNotas; i++)
+            {
+                notas.Add(valor);
+            }
+        }
+
+    }
+}
---
+++ Lab1/Lab5/TestaCaixaEletronico.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.Lab5
+{
+    [TestFixture]
+    class TestaCaixaEletronico
+    {
+        [Test]
+        public void Deve_retornar_a_nota_da_quantidade_pedida()
+        {
+
+            var valorDoSaque = 100;
+
+            var caixa = new Caixa();
+
+            var notasRecebidas = caixa.Saque(valorDoSaque);
+
+            var notasEsperadas = new List<int>() { 100};
+            Assert.AreEqual(notasEsperadas, notasRecebidas);
+        }
+
+        [Test]
+        public void Deve_retornar_duas_notas_iguais_para_um_valor()
+        {
+            var valorDoSaque = 200;
+
+            var caixa = new Caixa();
+
+            var notasRecebidas = caixa.Saque(valorDoSaque);
+
+            List<int> notasEsperadas = new List<int>() { 100, 100};
+
+            Assert.AreEqual(notasEsperadas, notasRecebidas);
+        }
+
+        [Test]
+        public void Deve_retornar_tres_notas_diferentes_para_um_valor()
+        {
+            var valorDoSaque = 170;
+
+            var caixa = new Caixa();
+
+            var notasRecebidas = caixa.Saque(valorDoSaque);
+
+            List<int> notasEsperadas = new List<int>() { 100, 50, 20 };
+
+            Assert.AreEqual(notasEsperadas, notasRecebidas);
+        }
+        
+        [Test]
+        public void Deve_retornar_uma_lista_vazia_para_um_valor_que_nao_pode_ser_pago()
+        {
+            var valorDoSaque = 153;
+
+            var caixa = new Caixa();
+
+            var notasRecebidas = caixa.Saque(valorDoSaque);
+
+            List<int> notasEsperadas = new List<int>() { };
+
+            Assert.AreEqual(notasEsperadas, notasRecebidas);
+
+        }
+    }
+}
